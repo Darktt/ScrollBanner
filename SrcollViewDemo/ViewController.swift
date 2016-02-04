46fdef61f5743f0211a659b7dde48349c8a3767f
Eden Li <eden@realtime.tw>
2016-02-04 15:57:55 +0800

    Initial Commit

---
+++ SrcollViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SrcollViewDemo
+//
+//  Created by EdenLi on 2016/2/4.
+//  Copyright © 2016年 Darktt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
